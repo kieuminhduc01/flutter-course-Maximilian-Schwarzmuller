dfe93f24541365dbcaae37382633523c8bb9ea07
Henry <kieuminhduc01@gmail.com>
2025-09-25 10:02:10 +0700

    37. Building Custom Widgets: tao moi mot child component bang widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,31 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.deepPurple,
-                Colors.deepOrange,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello Duc',
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
-        ),
-      ),
+      home: Scaffold(body: GradientContainer()),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.deepPurple,
+            Colors.deepOrange,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello Duc',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
